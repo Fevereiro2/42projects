f23f374a02a8a22845fc85cb197366aa8ac9a7d8
pfeverei <pfeverei@student.42porto.com>
2023-12-26 03:11:02 +0000

    upload files c03

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,29 @@
+
+
+//#include  <unistd.h>
+#include <stdio.h>
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    unsigned int	i;
+
+    i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+        if (s1[i] > s2[i])
+        {
+            return (1);
+        }
+        if (s1[i] > s2[i])
+        {
+            return (-1);
+        }
+		i++;
+	}
+	return (0);
+}
+
+int main(void)
+{
+    printf("%d", ft_strncmp("Hello", "HelloWorld", 8));
+}
