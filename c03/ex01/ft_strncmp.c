3ce4b908bac92f04f0be98bcc398af651580d076
pfeverei <pfeverei@student.42porto.com>
2023-12-27 11:00:23 +0000

    upload new files and update c03

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -14,7 +14,7 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
         {
             return (1);
         }
-        if (s1[i] > s2[i])
+        if (s1[i] < s2[i])
         {
             return (-1);
         }
---
+++ c04/ex00/ft_strlen.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str);
+int main (void)
+{
+	printf("%i", ft_strlen("854dsjfksdlk"));
+}
