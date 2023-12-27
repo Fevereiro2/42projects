9b7bc82327b0d6236c63b2c20b701ba3ef96d188
pfeverei <pfeverei@student.42porto.com>
2023-12-27 12:24:50 +0000

    upload c04

--- c04/ex00/ft_strlen.c
+++ c04/ex00/ft_strlen.c
@@ -1,8 +1,18 @@
-
 #include <stdio.h>
-#include <unistd.h>
+//#include <unistd.h>
 
-int	ft_strlen(char *str);
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 int main (void)
 {
 	printf("%i", ft_strlen("854dsjfksdlk"));
