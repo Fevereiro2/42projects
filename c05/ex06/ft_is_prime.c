5522d059e384046403365a3099c0b33a934a16fc
pfeverei <pfeverei@student.42porto.com>
2023-12-28 03:01:43 +0000

    upload files c05

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= (nb / 2))
+	{
+		if (!(nb % i))
+			return (0);
+		else
+			i += 1;
+	}
+	return (1);
+}
+
+int main (void)
+{
+	printf("%d -> %d\n", 0, ft_is_prime(0));
+	printf("%d -> %d\n", 1, ft_is_prime(1));
+	printf("%d -> %d\n", 2, ft_is_prime(2));
+}
